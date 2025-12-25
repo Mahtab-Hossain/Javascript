165f096b4c854d8703230b579c0066cc8861383e
Mahtab-Hossain <mahtab.forge@gmail.com>
2025-12-26 00:17:47 +0600

    fix: fixed minor bug in logic

--- temp_advice.js
+++ temp_advice.js
@@ -4,11 +4,11 @@ if (temp > 40) {
     console.log("Too hot");
 }else if (temp >= 30 && temp <= 40) {
     console.log("Hot Weather");
-}else if (temp >= 20 && temp <= 30) {
+}else if (temp >= 20 && temp <= 29) {
     console.log("Nice Weather");
-}else if (temp >= 10 && temp <= 20) {
+}else if (temp >= 10 && temp <= 19) {
     console.log("Cold weather");
-}else if (temp < 10) {
+}else if (temp < 10 && temp >= -273) {
     console.log("Too cold");
 }else{
     console.log("Inavlid Number...");
