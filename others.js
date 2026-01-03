d79fc826a1482bd1a7543fc6c80d7a91b1800243
Mahtab-Hossain <mahtab.forge@gmail.com>
2026-01-03 21:11:11 +0600

    added tax calculation function

--- others.js
+++ others.js
@@ -4,3 +4,14 @@ function calculateArea(lenght, width){
 }
 console.log(calculateArea(5, 10)); // Output: 50
 
+//Tax Calculation Function
+function calculateTax(amount){
+    var tax = 0;
+    if (amount > 10000) {
+        tax = amount * 0.10; // 10% tax
+    }else{
+        tax = amount * 0.05; //5% tax
+    }
+    return tax;
+}
+console.log(calculateTax(15000)); // Output: 1500
